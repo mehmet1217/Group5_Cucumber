47a1105ab17da946de35cbf49b4eeddf2dba3ce0
Emre BALCI <116108168+e1982b@users.noreply.github.com>
2023-05-03 15:34:21 +0200

    Update VerifyProductQuantityInCart.java

--- src/test/java/com/kitapyurdu/step_definitions/VerifyProductQuantityInCart.java
+++ src/test/java/com/kitapyurdu/step_definitions/VerifyProductQuantityInCart.java
@@ -51,38 +51,4 @@ public class VerifyProductQuantityInCart {
         anyProduct.sepeteGit.click();
         Assert.assertEquals(cartPage.quantity.getText(), "4");
     }
-
-
-    /*
-   #public class  extends TestBase {
-#    @Test
-#    public void verifyProductQuantityInCart() {
-#        extentLogger = report.createTest("Kitap Yurdu Add Products in Cart");
-#
-#
-#
-#        extentLogger.info("Verify that home page is visible successfully");
-#        Assert.assertEquals(anyProduct.anaSayfa.getText(),"Haftanın Yayınevi");
-#
-#        extentLogger.info("Click 'View Product' for any product on home page");
-#        BrowserUtils.hover(anyProduct.ürünSec);
-#        anyProduct.ürünSec.click();
-#
-#
-#        extentLogger.info("Click understand button");
-#        anyProduct.understandBtn.click();
-#        Assert.assertEquals(anyProduct.Roket1.getText(), "Kraliçeyi Kurtarmak");
-#
-#        extentLogger.info("Increase quantity to 4");
-#        int x = 0;
-#        for (x = 1; x <= 4; x++) {
-#            BrowserUtils.hover(anyProduct.sepeteEkle);
-#            anyProduct.sepeteEkle.click();
-#        }
-#        extentLogger.info("Click 'Add to cart' button");
-#        BrowserUtils.hover(anyProduct.sepetim);
-#        anyProduct.sepetim.click();
-#        anyProduct.sepeteGit.click();
-#        Assert.assertEquals(cartPage.quantity.getText(),"4");
-     */
 }
