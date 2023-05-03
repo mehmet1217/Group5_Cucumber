f4ea9a0c7f32039de058ea2674079dea3e60824d
Emre BALCI <116108168+e1982b@users.noreply.github.com>
2023-05-03 15:33:45 +0200

    Update AddProductsInCart_StepDef.java

--- src/test/java/com/kitapyurdu/step_definitions/AddProductsInCart_StepDef.java
+++ src/test/java/com/kitapyurdu/step_definitions/AddProductsInCart_StepDef.java
@@ -43,48 +43,4 @@ public class AddProductsInCart_StepDef {
         Assert.assertEquals(cartPage.totalFiyat.getText(), "44,69");
     }
 
-
-    /*
-    //public class AddProductsInCart extends TestBase {
-
-@Test
-public void addProductInCart() throws InterruptedException {
-extentLogger = report.createTest("");
-
-
-CocukKitaplari cocukKitaplari=new CocukKitaplari();
-CartPage cartPage=new CartPage();
-extentLogger.info("Click understand button");
-cocukKitaplari.understandBtn.click();
-
-extentLogger.info("");
-BrowserUtils.hover(cocukKitaplari.cocukKitaplari);
-cocukKitaplari.cocukKitaplari.click();
-
-
-BrowserUtils.hover(cocukKitaplari.firstBook);
-cocukKitaplari.firstBook.click();
-
-
-extentLogger.info("Hover the second product and click add to cart button");
-BrowserUtils.hover(cocukKitaplari.secondBook);
-cocukKitaplari.secondBook.click();
-extentLogger.info("Hover the cart and click 'Sepete Git' button");
-BrowserUtils.hover(cocukKitaplari.sepetim);
-cocukKitaplari.sepetim.click();
-
-Thread.sleep(3000);
-
-extentLogger.info("Hover the cart and click 'Sepete Git' button");
-BrowserUtils.hover(cocukKitaplari.sepeteGitBtn);
-cocukKitaplari.sepeteGitBtn.click();
-
-extentLogger.info("Verify their prices, quantity and total price");
-Assert.assertEquals(cartPage.ÜrünSayisi.getText(), "2");
-Assert.assertEquals(cartPage.totalFiyat.getText(), "43,94");
-
-
-}
-}
-     */
 }
