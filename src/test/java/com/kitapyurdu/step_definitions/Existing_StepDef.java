2d641bdb38247fc217f3c8a56e1c75020ac195c2
Yusuf <ysfkrds.63@gmail.com>
2023-05-08 14:13:17 +0200

    yusuf's part is uploaded

--- src/test/java/com/kitapyurdu/step_definitions/Existing_StepDef.java
+++ src/test/java/com/kitapyurdu/step_definitions/Existing_StepDef.java
@@ -9,7 +9,6 @@ import org.junit.Assert;
 
 public class Existing_StepDef {
     UyeolPage uyeolPage=new UyeolPage();
-
     @Given("As a user I am  on the Website")
     public void as_a_user_i_am_on_the_website() {
         uyeolPage.understandBtn.click();
@@ -27,10 +26,8 @@ public class Existing_StepDef {
     public void as_a_user_i_fill_and_and_and_and(String usernameyusuf, String userlastnameYsf, String usernameEmailYsf, String userpasswordYsf, String userpassword2) {
         uyeolPage.register(usernameyusuf,userlastnameYsf,usernameEmailYsf,userpasswordYsf,userpassword2);
     }
-
     @Then("As a user I should be see message")
     public void as_a_user_i_should_be_see_message(String expectedmessage) {
         Assert.assertEquals(expectedmessage,uyeolPage.existingMailAdress.getText());
-
     }
 }
